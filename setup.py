7ba0425f0ea156036f7f2b4debc68d3dd35ed32d
Kevin Garcia <d.kevin0402@gmail.com>
2023-01-09 15:49:16 -0600

    fix: update files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(
-    name='regression_plots', version='1.0', scripts=['regression_plots.py'],
+    name='regression_plots', version='1.0', scripts=['regression_plots/regression_plots.py'],
     author='Kevin Garcia', author_email='d.kevin0402@gmail.com',
     description='package that works as a tool in linear regression models and shows diagnostic graphs of the model.',
     url='https://github.com/kevillo/regression_plots', packages=setuptools.find_packages(),
